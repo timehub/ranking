7809e81806562c058104d14a92d4d727b61926d0
Nicolas Hock Isaza <nhocki@gmail.com>
2011-08-25 18:59:47 -0500

    Making it work with the avg

--- ranking.rb
+++ ranking.rb
@@ -81,7 +81,7 @@ class App < ActiveRecord::Base
   end
 
   def public_number_of_voters
-    (public_total_stars / 20.0).round(2)
+    (public_total_stars.to_d / public_average_stars.to_f).round(2)
   end
   
   def public_total_stars
